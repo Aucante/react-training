7dd32de4c06bd4541e8c5482ec1105381283df5a
Aucante <sebastien.aucante@gmail.com>
2022-10-06 20:17:37 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+import {useState} from 'react'
 import './App.css';
 import Item from './Item';
 
 function App() {
+
+  const [monState, setMonState] = useState(10);
   return (
     <div className="App">
       <h1>Hello</h1>
       <Item />
+      {monState}
     </div>
   );
 }
