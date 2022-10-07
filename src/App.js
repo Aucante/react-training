e34cb6df6ac7d0f8558985968f62d447fce7cf1b
Aucante <sebastien.aucante@gmail.com>
2022-10-07 21:37:36 +0200

    modifier state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import {useState} from 'react'
 import './App.css';
-import Item from './Item';
 
 function App() {
 
   const [monState, setMonState] = useState(10);
+
+  const modifyState = () => {
+    setMonState(20);
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Item />
-      {monState}
+      <h1>Hello state : {monState}</h1>
+      <button onClick={modifyState}>Change state </button>
     </div>
   );
 }
