8fe7530930d239c1690f4de91f7c1a4fadb58922
Aucante <sebastien.aucante@gmail.com>
2022-10-08 22:51:10 +0200

    input with react

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Item from './Components/Item/Item';
 
 function App() {
 
+  const [inputData, setInputData] = useState(10);
+
+  const changeInput = (e) => {
+    setInputData(e)
+  }
+
+  console.log(inputData);
+
   return (
     <div className="App">
       <h1>Hello state depuis App</h1>
-      <Item />
+      <input type="text" value={inputData} onInput={e => changeInput(e.target.value)} />
     </div>
   );
 }
