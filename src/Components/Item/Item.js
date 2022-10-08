0acb3b6f2437edc2ecde69ebee47e86c471d7801
Aucante <sebastien.aucante@gmail.com>
2022-10-08 22:40:49 +0200

    images with react

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -3,12 +3,10 @@ import './Item.css'
 
 function Item (){
 
-    const toggle = false;
-    const styleCSS = {color: toggle ? "greenyellow" : "blue", fontSize: "50px"}
-
     return (
         <div>
             <h1 class="titre-item">Hello item</h1>
+            <img src={'/building.jpg'} alt="" />
         </div>
     )     
 }
